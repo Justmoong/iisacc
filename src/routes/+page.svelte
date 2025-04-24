c95b44d6ab82148e0d4b6caa6bb02eef4bf0c698
Justmoong <andudyun0504@gmail.com>
2025-04-24 13:39:25 +0900

    Migrate to SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Card from '../lib/UIComponents/Card.svelte';
+	let Title = 'IISACC';
+  </script>
+  
+  <contents>
+	<h1 class="HeaderTitle">{Title}</h1>
+	<div class="CardContainer">
+	  <Card title="Title" description="Description" />
+	  <Card title="Title" description="Description" />
+	  <a href="https://www.google.com"><Card title="Title" description="Description" /></a>
+	</div>
+  </contents>
+  
+  <style>
+	contents {
+	  text-align: center;
+	  padding: 1em;
+	  margin: 0 auto;
+	}
+  
+	.HeaderTitle {
+	  font-family: 'Pretendard Bold', sans-serif;
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  padding: 2rem 0;
+	}
+  
+	.CardContainer {
+	  display: flex;
+	  flex-wrap: wrap;
+	  justify-content: center;
+	  gap: 1rem;
+	}
+  </style> 
