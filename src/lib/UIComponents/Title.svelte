82bac2ef4792f31812c55ca43117ef05916a8d3b
Justmoong <andudyun0504@gmail.com>
2025-05-11 00:02:39 +0900

    Add files

---
+++ src/lib/UIComponents/Title.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let title = ""
+</script>	
+
+<contents>
+	{title}
+</contents>
+
+<style>
+	contents {
+		font-size: 64px;
+		font-family: 'Pretendard Bold', sans-serif;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 128px;
+			color: var(--color-text-primary)
+	}
+</style>
