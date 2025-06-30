e0d1efe2d693ff3af2d989eeed29caf2f58a0a47
Justmoong <andudyun0504@gmail.com>
2025-06-30 12:22:58 +0900

    New Now Posts Logics for Sync Multiple Posts

--- src/lib/Layouts/PostLayout.svelte
+++ src/lib/Layouts/PostLayout.svelte
@@ -8,8 +8,13 @@
 </div>
 
 <style>
+
     :global(.blog-container) {
         align-items: left;
+				max-width: 720px;
+        height: auto;
+        display: block;
+        margin: 0 auto;
     }
 
     :global(.blog-container h1) {
@@ -21,4 +26,13 @@
 		:global(.blog-container p) {
 				padding-bottom: 24px;
 		}
+
+    :global img {
+        width: auto;
+        max-width: 100%;
+        object-fit: contain;
+        display: block;
+        margin: 0 auto;
+    }
+
 </style>
