479989c4756cd57e42dfeb87c7a381385975a25a
Justmoong <andudyun0504@gmail.com>
2025-07-08 21:59:56 +0900

    Processing Footer

--- src/lib/Layouts/Footer.svelte
+++ src/lib/Layouts/Footer.svelte
@@ -3,6 +3,14 @@
 </script>
 
 <div class="contents">
+	<div class="vstack">
+		<div class="translate-warn">
+				<img class="translate-warn-icon" src="/icons/warning.svg" alt="Translation Warning">
+			<p class="translate-warn-text">
+				Sometimes, I Use ChatGPT for translation and i know it causes inconsistencies.
+			</p>
+		</div>
+</div>
 
 </div>
 
@@ -12,7 +20,32 @@
 			align-items: center;
 			display: flex;
 			flex-direction: column;
-			justify-content: center;
 			width: 100%;
+			margin-top: 64px;
 	}
+
+  .vstack {
+      display: flex;
+      flex-direction: column;
+  }
+
+  .translate-warn {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+			gap: 32px;
+  }
+
+	.translate-warn-icon {
+      width: 32px;
+      height: auto;
+      object-fit: contain;
+			color: white;
+	}
+
+	.translate-warn-text {
+      margin-block-end: 0;
+      margin-block-start: 0;
+	}
+
 </style>
