96e7a67b7dfcda69137d2cc21209b0c61a5c42ee
Justmoong <andudyun0504@gmail.com>
2025-07-23 10:42:28 +0900

    New Post

--- src/routes/Posts/+page.svelte
+++ src/routes/Posts/+page.svelte
@@ -6,27 +6,39 @@
 
 
 <Header />
+
 <div class="posts-container">
-	<PostCard
-		title="Less Internet, Local First"
-		date="2025-07-16"
-		path="/Posts/Less Internet, Local First"
-	/>
+
+<PostCard
+	title="Infinite Depression"
+	date="2025-07-23"
+	path="/Posts/Infinite Depression"
+/>
+
+<PostCard
+	title="Less Internet, Local First"
+	date="2025-07-16"
+	path="/Posts/Less Internet, Local First"
+/>
+
 <PostCard
 	title="The paradox of technological progress"
 	date="2025-06-12"
 	path="/Posts/The paradox of technological progress"
 />
+
 <PostCard
 	title="Can Trump realize MAGA?"
 	date="2025-06-01"
 	path="/Posts/Can Trump realize MAGA"
 />
+
 <PostCard
 	title="The Illusion of Professional Activity"
 	date="2025-05-28"
 	path="/Posts/The Illusion of Professional Activity"
 />
+
 <Footer />
 
 </div>
---
+++ src/routes/Posts/Infinite Depression/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Post from '$lib/Posts/Infinite Depression.svx';
+</script>
+
+<Post />
+
+<style>
+
+</style>
