bd70f9fa0b2e9cbc1b452a9fe6ecf58f16b8a017
Justmoong <andudyun0504@gmail.com>
2025-08-13 20:06:49 +0900

    Fix URI error

--- src/routes/Apps/+page.svelte
+++ src/routes/Apps/+page.svelte
@@ -5,7 +5,7 @@
 
 <Header/>
 
-<a href="/src/routes/Apps/TimeScopes">
+<a href="/Apps/TimeScopes">
 <h2>Time Scopes</h2>
 	<p>
 		Time Scopes is a life time tracker. It shows to you remaining your lifetime and other informations
