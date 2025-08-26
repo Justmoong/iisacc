fa252c8a58ee0f128cc5a590a909fdf3d5f0f5a0
Justmoong <andudyun0504@gmail.com>
2025-08-27 08:27:27 +0900

    Fix Path Issue

--- src/routes/Store/+page.svelte
+++ src/routes/Store/+page.svelte
@@ -5,7 +5,7 @@
 
 <Header/>
 
-<a href="/Apps/TimeScopes">
+<a href="/Store/TimeScopes">
 <h2>Time Scopes</h2>
 	<p>
 		Time Scopes is a life time tracker. It shows to you remaining your lifetime and other informations
